0b7fb3baaf6018b77af50f13af356a0d18fe2cef
Muhammad Ashir Khan <muhammadashirkhan373@gmail.com>
2025-07-08 03:53:33 -0700

    Frontend setup react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
 
 const App = () => {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/products" element={<Products />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
